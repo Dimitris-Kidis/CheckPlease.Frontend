ded5c1f34fe52f60c1c11e4ec0c364c453eb4089
Dmitrii <dmitrii.romanenco@amdaris.com>
2023-07-16 21:33:49 +0300

    Init

---
+++ backend/CheckPlease/ApplicationCore/Domain/CheckPleaseDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore;
+using ApplicationCore.Domain.Entities;
+
+namespace ApplicationCore.Domain
+{
+
+    public class CheckPleaseDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Repair> Repairs { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Detail> Details { get; set; }
+
+
+        public CheckPleaseDbContext()
+        {
+
+        }
+
+        public CheckPleaseDbContext(DbContextOptions<CheckPleaseDbContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Data Source=.;Database=CheckPlease;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+    }
+}
