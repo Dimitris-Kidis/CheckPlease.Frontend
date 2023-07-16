f06c7e8bbad084f65ae449d94ea722533db263f1
Dmitrii <dmitrii.romanenco@amdaris.com>
2023-07-16 21:43:56 +0300

    Init

---
+++ backend/CheckPlease/Command/Car/CreateCar/CreateCarCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationCore.Services.Repository.CheckRepository;
+using MediatR;
+using ApplicationCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Car.CreateCar
+{
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, int>
+    {
+        private readonly ICheckRepository<ApplicationCore.Domain.Entities.Car> _carRepository;
+        public CreateCarCommandHandler(ICheckRepository<ApplicationCore.Domain.Entities.Car> carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public Task<int> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            var newCar = new ApplicationCore.Domain.Entities.Car
+            {
+                CarSign = request.CarSign,
+                VinCode = request.VinCode,
+                Mileage = request.Mileage,
+                Year = request.Year,
+                Volume = request.Volume,
+                Brand = request.Brand,
+                Model = request.Model
+            };
+
+            _carRepository.Add(newCar);
+            _carRepository.Save();
+
+            var resultId = newCar.Id;
+            return Task.FromResult(resultId);
+        }
+    }
+}
